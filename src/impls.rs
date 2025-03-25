71955d2c1f9b7e88ebd42894896cab6dd8e083e3
rust-master <muhammadzaryabrafique@gmail.com>
2025-03-25 14:37:18 +0500

    first commit

---
+++ src/impls.rs
@@ -0,0 +1,35 @@
+use super::*;
+use frame::{prelude::*, traits::{BlakeTwo256, Hash}};
+
+impl<T: Config> Pallet<T> {
+
+	pub fn gen_dna() -> [u8; 32]{
+		let unique_payload = (
+			frame_system::Pallet::<T>::parent_hash(),
+			frame_system::Pallet::<T>::block_number(),
+			frame_system::Pallet::<T>::extrinsic_index(),
+			CountForKitties::<T>::get(),
+		);
+
+		let hash: [u8; 32] = BlakeTwo256::hash_of(&unique_payload).into();
+
+		return hash;
+	}
+
+	pub fn mint(owner: T::AccountId, dna: [u8; 32]) -> DispatchResult {
+		let kitty = Kitty { dna, owner: owner.clone() };
+
+		ensure!(!Kitties::<T>::contains_key(dna), Error::<T>::DuplicateKitty);
+
+		let current_count: u32 = CountForKitties::<T>::get();
+		let new_count = current_count.checked_add(1).ok_or(Error::<T>::TooManyKitties)?;
+
+		KittiesOwned::<T>::append(&owner, dna);
+		Kitties::<T>::insert(dna, kitty);
+		CountForKitties::<T>::set(new_count);
+
+
+		Self::deposit_event(Event::<T>::Created { owner });
+		Ok(())
+	}
+}
